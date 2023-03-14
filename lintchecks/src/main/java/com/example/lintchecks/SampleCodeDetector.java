4d42b8b106d3d481982ccd3f3ad9b638c9ef1f0e
hdbamobileteam2 <61041132+hdbamobileteam2@users.noreply.github.com>
2023-03-14 17:24:52 +0800

    Added Custom Lint Checks

---
+++ lintchecks/src/main/java/com/example/lintchecks/SampleCodeDetector.java
@@ -0,0 +1,60 @@
+package com.example.lintchecks;
+
+import com.android.tools.lint.client.api.UElementHandler;
+import com.android.tools.lint.detector.api.Category;
+import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Implementation;
+import com.android.tools.lint.detector.api.Issue;
+import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.LintFix;
+import com.android.tools.lint.detector.api.Scope;
+import com.android.tools.lint.detector.api.Severity;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.uast.UElement;
+import org.jetbrains.uast.ULiteralExpression;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SampleCodeDetector extends Detector implements Detector.UastScanner {
+    @Override
+    public List<Class<? extends UElement>> getApplicableUastTypes() {
+        return Collections.singletonList(ULiteralExpression.class);
+    }
+    @Override
+    public UElementHandler createUastHandler(@NotNull JavaContext context) {
+
+        return new UElementHandler() {
+            @Override
+            public void visitLiteralExpression(@NotNull ULiteralExpression node) {
+                String string = node.getValue().toString();
+                if (string.contains("lint")) {
+                    context.report(
+                            javaLintIssue, node, context.getLocation(node),
+                            "This code mentions `lint`: **Congratulations!!!**",
+                            createFix()
+                    );
+                }
+            }
+        };
+    }
+
+    private LintFix createFix() {
+        return LintFix.create()
+                .replace()
+                .text("lint")
+                .with("new text")
+                .build();
+    }
+
+    public static Issue javaLintIssue = Issue.create(
+            "SampleId",
+            "Lint Mentions",
+            "This check highlights string literals in code which mentions the word `lint`. Blah blah blah. Another paragraph here.",
+            Category.MESSAGES,
+            6,
+            Severity.INFORMATIONAL,
+            new Implementation(SampleCodeDetector.class, Scope.JAVA_FILE_SCOPE)
+    );
+}
---
+++ lintchecks/src/main/java/com/example/lintchecks/SampleIssueRegistry.java
@@ -0,0 +1,48 @@
+package com.example.lintchecks;
+
+import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.detector.api.ApiKt;
+import com.android.tools.lint.detector.api.Issue;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SampleIssueRegistry extends IssueRegistry {
+    @NotNull
+    @Override
+    public List<Issue> getIssues() {
+        ArrayList<Issue> list = new ArrayList<>();
+        list.addAll(Collections.singletonList(ButtonDetector.buttonIssue));
+        list.addAll(Collections.singletonList(CustomButtonDetector.wrongMarginIssue));
+        list.addAll(Collections.singletonList(CustomButtonDetector.noMarginIssue));
+        list.addAll(Collections.singletonList(CustomButtonDetector.redundantMarginIssue));
+        list.addAll(Collections.singletonList(SampleCodeDetector.javaLintIssue));
+        list.addAll(Collections.singletonList(SampleMethodDetector.lintMethodIssue));
+        list.addAll(Collections.singletonList(SampleMethodDetector.lintMethodParameterIssue));
+        list.addAll(Collections.singletonList(SampleMethodDetector.lintMethodCommentIssue));
+        return list;
+    }
+
+    @Override
+    public int getApi() {
+        return ApiKt.CURRENT_API;
+    }
+
+//    @Override
+//    public int getMinApi() {
+//        return 8;
+//    }
+
+//    @Override
+//    public Vendor getVendor() {
+//        return new Vendor(
+//                "Android Open Source Project",
+//                "",
+//                "https://github.com/googlesamples/android-custom-lint-rules/issues",
+//                "https://github.com/googlesamples/android-custom-lint-rules"
+//        );
+//    }
+}
